a50db5de4d5ed23581a8be073127f0118b6138df
innovace <innovacegit@gmail.com>
2018-01-20 17:20:11 +0530

    Initial commit

---
+++ learnjava/src/main/java/com/example/learnjava/learnstring.java
@@ -0,0 +1,13 @@
+package com.example.learnjava;
+
+public class learnstring {
+    public static void main(String[] args) {
+
+        String name = "soundharya";
+        int age = 23;
+        float bankBalance = 22.300f;
+        float finalBalance = bankBalance - 50;
+
+        System.out.printf("Name :" +name+ "age "+age);
+    }
+}
